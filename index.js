0b75e236b779ee249b0d01d971a79e48be9956b2
skaadin <aritroshome.development@gmail.com>
2021-01-09 13:35:29 +0530

    Comitted

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const nedb = require('nedb');
+
+const db = new nedb({filename: 'db.json'});
+
+db.loadDatabase();
+
+const app = express();
+
+const PORT = 3000;
+
+app.set('views', path.join(__dirname, 'public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (request, response) => {
+    response.render('index');
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server started at https://localhost:${PORT}`);
+})
