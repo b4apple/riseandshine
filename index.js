23ecd7857756389a6da28959ca80e1b5c94c06ee
skaadin <aritroshome.development@gmail.com>
2021-01-09 15:27:17 +0530

    all

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const bodyparser = require('body-parser')
 const nedb = require('nedb');
+const { request } = require('http');
+const { response } = require('express');
 
-const db = new nedb({filename: 'db.json'});
+const userdb = new nedb({ filename: 'db.json' });
 
-db.loadDatabase();
+userdb.loadDatabase();
 
 const app = express();
 
@@ -13,12 +15,21 @@ const PORT = 3000;
 
 app.set('views', path.join(__dirname, 'public'));
 app.set('view engine', 'ejs');
+app.use(bodyparser.json());
+
 
 app.get('/', (request, response) => {
     response.render('index');
 });
 
-[]
+app.post('/login', (request, response) => {
+    
+});
+
+app.post('/signup', (request, response) => {
+    userdb.insert({ name: request.params.name, password: request.params.password });
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server started at https://localhost:${PORT}`);
